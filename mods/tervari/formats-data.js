5075c6d080ddab11d4dc3a1bff528a23471a5433
Oiawesome <Oiawesome99@yahoo.com>
2013-07-06 10:57:57 -0700

    add shit

--- mods/tervari/formats-data.js
+++ mods/tervari/formats-data.js
@@ -1,1 +1,42 @@
-//this is for testing iirc
+exports.BattleFormatsData = {
+        viterb: {
+                viableMoves: {"stoneedge":1,"leafblade":1,"leechseed":1,"substitute":1,"swordsdance":1,"agility":1},
+                dreamWorldRelease: true,
+                tier: "LC"
+        },
+        hierbado: {
+        viableMoves: {"stoneedge":1,"leafblade":1,"leechseed":1,"substitute":1,"swordsdance":1,"agility":1},
+                dreamWorldRelease: true,
+                tier: "NFE"
+        },
+        rapagram: {
+                viableMoves: {"stoneedge":1,"woodhammer":1,"leafblade":1,"leechseed":1,"earthquake":1,"rockpolish":1,"swordsdance":1},
+                dreamWorldRelease: true,
+                tier: "OU"
+        },
+        gazceit: {
+                viableMoves: {"substitute":1,"batonpass":1,"swordsdance":1,"return":1,"voidhorn":1,"doublekick":1,"quickattack":1,"wildcharge":1,"hornleech":1,"flamecharge":1,"headsmash":1},
+                dreamWorldRelease: true,
+                tier: "OU"
+        },
+        ninjantis: {viableMoves: {"uturn":1,"hijumpkick":1,"swordsdance":1,"xscissor":1,"substitute":1},
+                dreamWorldRelease: true,
+                tier: "LC"
+        },
+        manjitsu: {viableMoves: {"uturn":1,"hijumpkick":1,"swordsdance":1,"xscissor":1,"substitute":1},
+                dreamWorldRelease: true,
+                tier: "RU"
+        },
+        witcheance: {viableMoves: {"spore":1,"calmmind":1,"thunderbolt":1,"vendetta":1,"energyball":1,"destinybond":1,"willowisp":1,"focusblast":1},
+                dreamWorldRelease: true,
+                tier: "OU"
+        },
+        aguilari: {viableMoves: {"rockblast":1,"divingtalon":1,"pursuit":1,"bulkup":1,"stormthrow":1,"circlethrow":1,"aerialace":1,"bravebird":1},
+                dreamWorldRelease: true,
+                tier: "OU"
+        },
+        komolten: {viableMoves: {"firerocket":1,"dragondance":1,"earthquake":1,"flareblitz":1,"bulkup":1,"wildcharge":1,"stoneedge":1},
+                dreamWorldRelease: true,
+                tier: "UU"
+        }
+};//this is for testing iirc
